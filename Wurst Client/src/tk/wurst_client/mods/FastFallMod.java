395a2d40110bcce015edaaa0889633dc2dbe8875
MrAnyone <m3rc3r99@gmail.com>
2016-03-05 16:39:46 -0300

    Add a FastFall Setting
    Fast falling in water can be very annoying in survival mode, but
    sometimes is useful.

--- Wurst Client/src/tk/wurst_client/mods/FastFallMod.java
+++ Wurst Client/src/tk/wurst_client/mods/FastFallMod.java
@@ -10,12 +10,16 @@ package tk.wurst_client.mods;
 import tk.wurst_client.events.listeners.UpdateListener;
 import tk.wurst_client.mods.Mod.Category;
 import tk.wurst_client.mods.Mod.Info;
+import tk.wurst_client.navigator.settings.CheckboxSetting;
 
 @Info(category = Category.MOVEMENT,
 	description = "Makes you fall very fast.",
 	name = "FastFall")
 public class FastFallMod extends Mod implements UpdateListener
 {
+	public final CheckboxSetting fastFallOnWater = new CheckboxSetting(
+		"Fast fall on water", false);
+	
 	@Override
 	public void onEnable()
 	{
@@ -23,10 +27,23 @@ public class FastFallMod extends Mod implements UpdateListener
 	}
 	
 	@Override
+	public void initSettings()
+	{
+		settings.add(fastFallOnWater);
+	}
+	
+	@Override
 	public void onUpdate()
 	{
+		if(mc.thePlayer.isInWater() && !fastFallOnWater.isChecked())
+		{
+			return;
+		}
+		
 		if(!mc.thePlayer.onGround && !mc.thePlayer.isOnLadder()
-			&& !wurst.mods.flightMod.isEnabled())
+			&& !mc.thePlayer.capabilities.isFlying
+			&& !wurst.mods.flightMod.isEnabled()
+			&& !wurst.mods.safeWalkMod.isEnabled())
 		{
 			mc.thePlayer.motionY -= 5;
 		}
