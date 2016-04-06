21ecb9988ce6296f851d7865d13530d56efdc6fd
ThisTestUser <testsubjectforka1@gmail.com>
2016-04-06 19:41:49 -0400

    Add pingspoof modes

--- Wurst Client/src/tk/wurst_client/mods/PingSpoofMod.java
+++ Wurst Client/src/tk/wurst_client/mods/PingSpoofMod.java
@@ -7,18 +7,49 @@
  */
 package tk.wurst_client.mods;
 
+import org.darkstorm.minecraft.gui.component.BoundedRangeComponent.ValueDisplay;
+
 import net.minecraft.network.play.client.C00PacketKeepAlive;
 import tk.wurst_client.events.PacketOutputEvent;
 import tk.wurst_client.events.listeners.PacketOutputListener;
 import tk.wurst_client.mods.Mod.Category;
 import tk.wurst_client.mods.Mod.Info;
+import tk.wurst_client.navigator.settings.ModeSetting;
+import tk.wurst_client.navigator.settings.SliderSetting;
 
 @Info(category = Category.EXPLOITS,
 	description = "Spoofs ping to a low amount. While the key mode\n"
-		+ "is excellent if it works, the delay mode works absolute. ",
+		+ "is excellent if it works, the packet mode works 100%.",
 	name = "PingSpoof")
 public class PingSpoofMod extends Mod implements PacketOutputListener
 {
+	private int mode = 0;
+	private String[] modes = new String[]{"Key", "Packet"};
+	private long packetdelay;
+	
+	@Override
+	public void initSettings()
+	{
+		settings.add(new SliderSetting("Packet Delay", packet, 1, 20, 0.5,
+			ValueDisplay.DECIMAL)
+		{
+			@Override
+			public void update()
+			{
+				packet = (float)getValue();
+				packetdelay = (long)(packet * 1000);
+			}
+		});
+		settings.add(new ModeSetting("Mode", modes, mode)
+		{
+			@Override
+			public void update()
+			{
+				mode = getSelected();
+			}
+		});
+	}
+	
 	@Override
 	public void onEnable()
 	{
@@ -28,11 +59,24 @@ public class PingSpoofMod extends Mod implements PacketOutputListener
 	@Override
 	public void onSentPacket(PacketOutputEvent event)
 	{
+		if(mode == 0)
+		{
 		 PacketOutputEvent sent = (PacketOutputEvent)event;
          if(sent.getPacket() instanceof C00PacketKeepAlive) {
             C00PacketKeepAlive packet = (C00PacketKeepAlive)sent.getPacket();
             packet.key = Integer.MAX_VALUE;
          }
+		}
+		if(mode == 1)
+		{
+			updateMS();
+			if (!(event.getPacket() instanceof C00PacketKeepAlive))
+				return;
+			if (!hasTimePassedM(packetdelay))
+		            event.cancel();
+		        else
+		            updateLastMS();
+		}
 		
 	}
 	
