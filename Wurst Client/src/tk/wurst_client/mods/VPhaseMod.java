90a9d7a25acb554e396ee9ad57a32640d16c7f40
ThisTestUser <testsubjectforka1@gmail.com>
2016-04-24 12:16:46 -0400

    Fix VPhase

--- Wurst Client/src/tk/wurst_client/mods/VPhaseMod.java
+++ Wurst Client/src/tk/wurst_client/mods/VPhaseMod.java
@@ -9,57 +9,47 @@ package tk.wurst_client.mods;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockAir;
-import net.minecraft.network.play.client.C03PacketPlayer;
-import net.minecraft.network.play.server.S12PacketEntityVelocity;
+import net.minecraft.block.BlockHopper;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.MathHelper;
 import tk.wurst_client.events.BlockBBEvent;
-import tk.wurst_client.events.PacketInputEvent;
 import tk.wurst_client.events.listeners.BlockBBListener;
-import tk.wurst_client.events.listeners.PacketInputListener;
-import tk.wurst_client.events.listeners.PostUpdateListener;
+import tk.wurst_client.events.listeners.UpdateListener;
 import tk.wurst_client.mods.Mod.Category;
 import tk.wurst_client.mods.Mod.Info;
 
-@Info(category = Category.MOVEMENT,
-	description = "Phase through blocks vertically.\n"
-		+ "Can help you attach to walls.",
-	name = "VPhase")
-public class VPhaseMod extends Mod implements PostUpdateListener, PacketInputListener, BlockBBListener
+@Info(category = Category.EXPLOITS,
+	description = "Phase through blocks.\n"
+		+ "Tip: You can climb blocks with this.",
+	name = "VPhase",
+	tags = "CheckerClimb")
+public class VPhaseMod extends Mod implements BlockBBListener, UpdateListener
 {
 	@Override
 	public void onEnable()
 	{
-		wurst.events.add(PacketInputListener.class, this);
-		wurst.events.add(PostUpdateListener.class, this);
+		wurst.events.add(UpdateListener.class, this);
 		wurst.events.add(BlockBBListener.class, this);
-	}
-	
-	@Override
-	public void onPostUpdate()
-	{
-		if(!isInsideBlock())
-		mc.getNetHandler().addToSendQueue(new C03PacketPlayer.C04PacketPlayerPosition(
-			mc.thePlayer.posX, mc.thePlayer.posY - 0.02D, mc.thePlayer.posZ, true));
-	}
-	
-	@Override
-	public void onReceivedPacket(PacketInputEvent event)
-	{
-		if(event.getPacket() instanceof S12PacketEntityVelocity && ((
-			S12PacketEntityVelocity)event.getPacket()).func_149412_c() ==
-			mc.thePlayer.getEntityId() && isInsideBlock())
-           event.cancel();
 	}
 	
 	@Override
 	public void onBlockBB(BlockBBEvent event)
 	{
-		if((double)event.getY() > mc.thePlayer.boundingBox.minY - 0.3D && (double)event.getY() 
-			< mc.thePlayer.boundingBox.maxY && !isInsideBlock() && mc.thePlayer.isCollidedHorizontally)
-            event.setBoundingBox((AxisAlignedBB)null);
-            
+		 if (event.getBoundingBox() != null && event.getBoundingBox().maxY > 
+		 mc.thePlayer.boundingBox.minY)
+		      event.setBoundingBox(null);
+	}
+	
+	@Override
+	public void onUpdate()
+	{
+		 if (isInsideBlock() && mc.thePlayer.isSneaking())
+	      {
+			 float yaw = mc.thePlayer.rotationYaw;
+			 mc.thePlayer.boundingBox.offsetAndUpdate(1.2 * Math.cos(Math.toRadians(yaw + 90.0F)),
+				 0.0D, 1.2 * Math.sin(Math.toRadians(yaw + 90.0F)));
+	      }
 	}
 	
 	private boolean isInsideBlock() 
@@ -76,6 +66,8 @@ public class VPhaseMod extends Mod implements PostUpdateListener, PacketInputLis
 					{
 						AxisAlignedBB boundingBox = block.getCollisionBoundingBox(mc.theWorld, 
 							new BlockPos(x, y, z), mc.theWorld.getBlockState(new BlockPos(x, y, z)));
+						 if (block instanceof BlockHopper)
+				              boundingBox = new AxisAlignedBB(x, y, z, x + 1, y + 1, z + 1);
 						if(boundingBox != null && mc.thePlayer.boundingBox.intersectsWith(boundingBox)) 
 							return true;
 	               }
@@ -87,8 +79,7 @@ public class VPhaseMod extends Mod implements PostUpdateListener, PacketInputLis
 	@Override
 	public void onDisable()
 	{
-		wurst.events.remove(PacketInputListener.class, this);
-		wurst.events.remove(PostUpdateListener.class, this);
+		wurst.events.remove(UpdateListener.class, this);
 		wurst.events.remove(BlockBBListener.class, this);
 	}
 }
